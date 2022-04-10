07480ec88db2406b6e66dab24e6c8abb200acc24
xinqi wang <xinqi.wang@stonybrook.edu>
2022-04-10 18:48:44 -0400

    three algo

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,9 @@
 /**
  * Dijkstra's Algorithms
  */ 
-void Dijkstra(std::vector<adjPair> *graph, int source, int n_nodes, int *dist, int *prev)
+void Dijkstra_pq(int** graph, int source, int n_nodes, int *dist, int *prev, int num_threads)
 {
-    printf("Computing using Dijkstra's Algorithm ...\n");
+    printf("Computing using Dijkstra's Algorithm with Priority Queue...\n");
 
     std::priority_queue<distPair, std::vector<distPair>,compare> pq;
     for(int i=0; i<n_nodes;i++){
@@ -34,10 +34,10 @@ void Dijkstra(std::vector<adjPair> *graph, int source, int n_nodes, int *dist, i
 
     std::vector<distPair>::iterator it;
     distPair p;
-    int u, v, alt, size, id, num;
+    int u, v, alt, id, num;
     std::vector<distPair> new_pairs;
 
-    # pragma omp parallel private (num, p, v, alt, new_pairs, id) shared (size, pq, u, graph, dist)
+    # pragma omp parallel private (num, p, v, alt, new_pairs, id) shared (pq, u, graph, dist)
     {
         id = omp_get_thread_num();
         num = omp_get_num_threads();
@@ -48,15 +48,14 @@ void Dijkstra(std::vector<adjPair> *graph, int source, int n_nodes, int *dist, i
                 p = pq.top(); 
                 u = p.first;
                 pq.pop();
-                size = graph[u].size();
+                // size = graph[u].size();
             }
             #pragma omp barrier
             
             // # pragma omp parallel
-            for(int i=id; i<size ; i+=num){
-                // int i = ind + id;
-                v = graph[u][i].first;
-                alt = dist[u] + graph[u][i].second;
+            for(int i=id; i<n_nodes ; i+=num){
+                v = i;
+                alt = dist[u] + graph[u][v];
                 // printf("%d u=%d v=%d alt=%d+%d=%d dist[v]=%d\n", 
                 //         id, u, v, dist[u], graph[u][i].second, alt, dist[v]);
                 if (alt<dist[v]){
@@ -80,34 +79,129 @@ void Dijkstra(std::vector<adjPair> *graph, int source, int n_nodes, int *dist, i
     }
 }
 
+void Dijkstra(int** graph, int source, int n_nodes, int *dist, int *prev, int num_of_threads)
+{
+    printf("Computing using Dijkstra's Algorithm...\n");
+    // omp_set_num_threads(num_of_threads);
+    // printf("asdasdasdasfasdasd\n");
+    // std::priority_queue<distPair, std::vector<distPair>, compare> pq;
+    int* visited = (int*)calloc(n_nodes, sizeof(int));
+    int* mindist = (int*)malloc(sizeof(int)*num_of_threads);
+    int* minInd = (int*)malloc(sizeof(int)*num_of_threads);
+
+    int finished = 0;
+    // int id, numThreads;
+    int global_min = 1000*n_nodes;
+    int u = 0;
+
+    #pragma omp parallel shared(finished, source, dist, visited, global_min, u, mindist, minInd, graph, n_nodes)
+    {
+        int id = omp_get_thread_num();
+        int numThreads = omp_get_num_threads();
+    
+        for(int i=id; i<n_nodes; i+=numThreads){
+            dist[i] = 1000*n_nodes;
+        }
+
+        #pragma omp single
+        {
+            dist[source] = 0;
+        }
+
+        for(; finished<n_nodes;)
+        {
+            int minDis = 1000*n_nodes;
+            int minNode = 0;
+            
+            // start = (id * n_nodes) / numThreads;
+            // end = ((id + 1) * n_nodes) / numThreads;
+
+            for (int i = id; i < n_nodes; i+=numThreads)
+            {
+                if (visited[i] == 0)
+                {
+                    if (dist[i] < minDis)
+                    {
+                        minDis = dist[i];
+                        minNode = i;
+                    }
+                }
+            }
+            // printf("%d %d minDis=%d minInd=%d\n", id, finished, minDis, minNode);
+            mindist[id] = minDis;
+            minInd[id] = minNode;
+            
+            #pragma omp single
+            {
+                global_min = 1000*n_nodes;
+                u = 0;
+                for(int t=0; t<numThreads; t++){
+                    if(mindist[t]<global_min){
+                        global_min = mindist[t];
+                        u = minInd[t];
+                    }
+                }
+                visited[u] = 1;
+            }
+            #pragma omp barrier
+
+            for (int v = id; v<n_nodes; v+=num_of_threads)
+            {
+                // int node = it->first;
+                if (visited[v] == 0)
+                {
+                    int alt = dist[u] + graph[u][v];
+                    // printf("%d u=%d v=%d alt=%d+%d=%d dist[v]=%d\n",
+                    //        id, u, v, dist[u], graph[u][v], alt, dist[v]);
+                    if (alt < dist[v])
+                    {
+                        dist[v] = dist[u] + graph[u][v];
+                        prev[v] = u;
+                    }
+                }
+            }
+            #pragma omp barrier
+            #pragma omp single
+            finished++;
+        }
+    }
+
+    free(visited);
+}
+
+
 /**
  * Bellman Ford Algorithm
  */
-void BellmanFord(std::vector<adjPair> *graph, int source, int n_nodes, int *dist, int *prev){
+void BellmanFord(int** graph, int source, int n_nodes, int *dist, int *prev){
     printf("Computing using Bellman Ford's Algorithm ...\n");
 
-    // for(int i=0; i<n_nodes;i++){
-    //     if(i==source)
-    //         dist[i] = 0;
-    //     else
-    //         dist[i] = 1000*n_nodes;
-    // }
 
-    // std::list<distPair>::iterator it;
-    // for (int i=0; i<n_nodes; i++){
-    //     for (int u=0; u<n_nodes; u++){
+    # pragma omp parallel shared (prev, graph, dist)
+    {
+        int id = omp_get_thread_num();
+        int num = omp_get_num_threads();
+        for(int i=id; i<n_nodes;i+=num){
+            if(i==source)
+                dist[i] = 0;
+            else
+                dist[i] = 1000*n_nodes;
+        }
 
-    //         for(it=graph[u].begin();it!=graph[u].end();it++){
-    //             int v = it->first;
-    //             int alt = dist[u] + it->second;
+        for (int i=0; i<n_nodes; i++){
+            for (int u=0; u<n_nodes; u++){
+                for(int v = id; v<n_nodes; v+=num){
+                    // int v = it->first;
+                    int alt = dist[u] + graph[u][v];
 
-    //             if (dist[u]!=1000*n_nodes && alt<dist[v]){
-    //                 dist[v] = alt;
-    //                 prev[v] = u;
-    //             }
-    //         }
-    //     }
-    // }
+                    if (dist[u]!=1000*n_nodes && alt<dist[v]){
+                        dist[v] = alt;
+                        prev[v] = u;
+                    }
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -176,14 +270,35 @@ int main(int argc, char *argv[])
 
     int n_nodes, n_edges;
     fscanf(input, "%d\t%d\n", &n_nodes, &n_edges);
-    std::vector<adjPair> *graph = new std::vector<adjPair>[n_nodes];
+    // std::vector<adjPair> *graph = new std::vector<adjPair>;
+    // std::vector<std::vector<adjPair>> graph(n_nodes);
+    int **graph = (int**)malloc(sizeof(int*)*n_nodes);
+    for (int i=0; i<n_nodes; i++){
+        graph[i]=(int*)malloc(sizeof(int)*n_nodes);
+        if(graph[i]==NULL){
+            printf("Unable to maloc graph[%d]\n", i);
+            return 1;
+        }
+    }
+    // printf("1done reading...\n"); 
     
+    for(int i=0; i<n_nodes; i++){
+        for (int j=0; j<n_nodes; j++){
+            if(i==j)
+                graph[i][j] = 0;
+            else
+                graph[i][j] = 1000*n_nodes;
+        }
+    }
+    // printf("2done reading...\n"); 
+
     int u, v, w;
     for(int i=0; i<n_edges; i++){
-        fscanf(input, "%d\t%d\t%d\n", &u, &v, &w);        
-        // printf("(%d\t%d)\t%d\n", u, v, w);
-        graph[u].push_back({v, w});
+        fscanf(input, "%d\t%d\t%d\n", &u, &v, &w); 
+        // printf("graph[%d][%d] = %d\n", u, v, w);  
+        graph[u][v] = w;
     }
+    // printf("3done reading...\n"); 
 
     init_time += duration_cast<dsec>(Clock::now() - init_start).count();
     printf("Initialization Time: %lf.\n", init_time);
@@ -198,15 +313,15 @@ int main(int argc, char *argv[])
     double compute_time = 0;
 
     if(algorithm=='d')
-        Dijkstra(graph, source, n_nodes, dist, prev);
+        Dijkstra_pq(graph, source, n_nodes, dist, prev, num_threads);
     else
         BellmanFord(graph, source, n_nodes, dist, prev);
     
     compute_time += duration_cast<dsec>(Clock::now() - compute_start).count();
-    printf("Computation Time: %lf.\n", compute_time);
+    printf("***Computation Time: %lf.\n", compute_time);
 
     /* DONE Write output */
-    printf("Writing output...\n");
+    // printf("Writing output...\n");
     char *inputname = (char*)malloc(sizeof(char)*strlen(inputPath));
     strcpy(inputname, inputPath);
     char *pt;
